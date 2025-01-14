4b1445c5a4c8cf106f1c250b9bd9e487df5266cb
Ronald <caraballo2036@gmail.com>
2025-01-13 19:24:06 -0500

    "Seguimos trabajando variables"

---
+++ Sistema de Tienda online.py
@@ -0,0 +1,11 @@
+print("***Sistema de Tienda online***")
+
+nombre_producto = "Camara Digital"
+precio_producto = 1500.99
+cantidad_productos = 3
+disponibilidad = True
+
+print("Nombre del producto: ", nombre_producto)
+print("Precio del producto: ", precio_producto)
+print("Cantidad del producto: ", cantidad_productos)
+print("Esta disponible: ", disponibilidad)
---
+++ Sistema reserva de hoteles.py
@@ -0,0 +1,23 @@
+nombre_cliente = "Pedro Perez"
+Dias_estancia = 5
+tarifa_dia = 5300.00
+vista = False
+
+
+
+print("Sistema de reserva de hoteles")
+
+print("Nombre del cliente: ", nombre_cliente)
+print("Dias de estancia: ", Dias_estancia)
+print("Tarifa de dia: ", tarifa_dia)
+print("La habitacion tiene vista al mar ?: ", vista)
+
+nombre_cliente = "Maria resrt"
+Dias_estancia = 8
+tarifa_dia = 5400.00
+vista = False
+
+print("Nombre del cliente: ", nombre_cliente)
+print("Dias de estancia: ", Dias_estancia)
+print("Tarifa de dia: ", tarifa_dia)
+print("La habitacion tiene vista al mar ?: ", vista)
