79bfca2a43b842d4a43ca8b752c0a8346e8b3e21
Ronald <caraballo2036@gmail.com>
2025-01-21 08:03:10 -0500

    "Seguimos trabajando variables"

---
+++ concatenacion_cadena.py
@@ -0,0 +1,10 @@
+cadena1 = "Hola"
+cadena2 = "Mundo"
+concatenacion = cadena1 + " "+ cadena2
+
+print(concatenacion)
+
+#Metodo join
+
+concatenacion = ''.join([cadena1, ' ', cadena2])
+print(concatenacion)
