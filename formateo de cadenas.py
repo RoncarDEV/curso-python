8451472658db0c3727d86138d0c24fa136007685
Ronald <caraballo2036@gmail.com>
2025-01-22 08:13:02 -0500

    "Seguimos trabajando variables"

---
+++ formateo de cadenas.py
@@ -0,0 +1,14 @@
+#FORMATEO DE CADENAS
+
+nombre = 'Juan'
+edad = 30
+
+# f-string
+
+mensaje = f'Hola me llamo {nombre} y tengo {edad} anos'
+print(mensaje)
+
+#mettodo antiguo format
+
+mensaje2= "Hola me llamo {} y tengo {} anos".format(nombre, edad)
+print(mensaje2)
